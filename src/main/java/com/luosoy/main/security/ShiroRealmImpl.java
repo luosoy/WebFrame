007d65bb4fa4adaa4b35fb27e2769aeb16eac03c
luozp <luozp@DESKTOP-EK6VEUS>
2016-12-22 09:10:00 +0800

    提交

--- src/main/java/com/luosoy/main/security/ShiroRealmImpl.java
+++ src/main/java/com/luosoy/main/security/ShiroRealmImpl.java
@@ -15,6 +15,7 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -48,22 +49,26 @@ public class ShiroRealmImpl extends AuthorizingRealm {
         String password = new String(token.getPassword());//密码
         QxUserCMP qxuser = userRepository.findByLoginname(username);
         if (qxuser != null) {
-            QxUserDTO qudto = new QxUserDTO();
-            BeanConvertUtil.convert(qxuser, qudto);
-            //组合username,两次迭代，对密码进行加密 
-            String pwdEncrypt = CipherUtil.createPwdEncrypt(username, password, qudto.getSalt());
-            if (qudto.getPassword().equals(pwdEncrypt)) {
-                AuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(qudto.getLoginname(), password, getName());
-                this.setSession(Const.SESSION_USER, qudto);
-                return authenticationInfo;
+            if (qxuser.getYxbz().equals('Y')) {
+                QxUserDTO qudto = new QxUserDTO();
+                BeanConvertUtil.convert(qxuser, qudto);
+                //组合username,两次迭代，对密码进行加密 
+                String pwdEncrypt = CipherUtil.createPwdEncrypt(username, password, qudto.getSalt());
+                if (qudto.getPassword().equals(pwdEncrypt)) {
+                    AuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(qudto.getLoginname(), password, getName());
+                    this.setSession(Const.SESSION_USER, qudto);
+                    return authenticationInfo;
+                } else {
+                    throw new IncorrectCredentialsException();
+                }
             } else {
-                throw new IncorrectCredentialsException();
+                throw new LockedAccountException();
             }
         } else {
             throw new UnknownAccountException();
         }
     }
-    
+
     private void setSession(Object key, Object value) {
         Subject currentUser = SecurityUtils.getSubject();
         if (null != currentUser) {
